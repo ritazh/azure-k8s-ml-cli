b484497f275d6312991ab0e7c035d6822469ab01
Rita Zhang <rita.z.zhang@gmail.com>
2017-06-13 09:24:04 -0700

    Add list of docker images and tags

---
+++ azk8sml/constants.py
@@ -0,0 +1,13 @@
+
+DOCKER_IMAGES = {
+    "cpu": {
+        "tensorflow": "ritazh/azk8sml-tensorflow:latest",
+    },
+    "gpu": {
+        "tensorflow": "ritazh/azk8sml-tensorflow:latest-gpu",
+    }
+}
+
+DEFAULT_DOCKER_IMAGE = "ritazh/azk8sml-tensorflow:latest"
+DEFAULT_ARCH = "cpu"
+
